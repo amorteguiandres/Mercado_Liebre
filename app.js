deae46f560470a51d2baf1ff7b0c9543c698642c
AndresAmortegui <andres.amortegui@gmail.com>
2023-08-08 22:59:56 -0500

    add

--- app.js
+++ app.js
@@ -4,22 +4,23 @@ const PORT = process.env.PORT||3001;
 const path = require('path');
 
 
+app.use(express.static(path.resolve(__dirname, './public')));
 
 app.get('/', (req, res) => {
-    let htmlPath = path.resolve(__dirname, '../views/home.html');
+    let htmlPath = path.resolve(__dirname, './views/home.html');
     res.sendFile(htmlPath);
 });
 
 app.get('/register', (req, res) => {
-    let htmlPath = path.resolve(__dirname, '../views/register.html');
+    let htmlPath = path.resolve(__dirname, './views/register.html');
     res.sendFile(htmlPath);
 });
 
 app.get('/login', (req, res) => {
-    let htmlPath = path.resolve(__dirname, '../views/login.html');
+    let htmlPath = path.resolve(__dirname, './views/login.html');
     res.sendFile(htmlPath);
 });
 
-app.use(express.static(path.resolve(__dirname, '../public')));
+
 
 app.listen(PORT, () => console.log('Escuchando en http://localhost:'+PORT));
